6242bb668bb4299040a0e81384d540ac3ecc4e72
aalleon <antoine.alleon@gmail.com>
2022-09-13 13:59:29 +0200

    first header version. mlx not included.

---
+++ includes/cub3d.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalleon <aalleon@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/13 13:50:08 by aalleon           #+#    #+#             */
+/*   Updated: 2022/09/13 13:59:09 by aalleon          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef _CUB3D_H_
+# define _CUB3D_H_
+
+# include <stdio.h>
+
+typedef struct s_vector
+{
+	float	x;
+	float	y;
+}	t_vector;
+
+typedef struct s_color
+{
+	unsigned char	R;
+	unsigned char	G;
+	unsigned char	B;
+}	t_color;
+
+typedef struct s_map
+{
+	unsigned int	height;
+	unsigned int	length;
+	char			**map;
+}	t_map;
+
+typedef struct s_character
+{
+	t_vector	position;
+	t_vector	direction;
+	float		camera_ratio;
+}	t_character;
+
+typedef struct s_global
+{
+	t_map		*map;
+	t_character	*pov;
+}	t_global;
+
+#endif
