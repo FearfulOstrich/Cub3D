778e7c6597d89f1c5bcf7166eb666513e4ac6aef
aalleon <antoine.alleon@gmail.com>
2022-09-21 14:24:48 +0200

    fixes.

--- src/raycasting/draw_column.c
+++ src/raycasting/draw_column.c
@@ -6,24 +6,23 @@
 /*   By: aalleon <aalleon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 12:01:35 by aalleon           #+#    #+#             */
-/*   Updated: 2022/09/21 13:28:09 by aalleon          ###   ########.fr       */
+/*   Updated: 2022/09/21 14:23:32 by aalleon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static unsigned int get_texture_color(t_texture texture, t_RC tools_RC,\
-										float h_ratio, float w_ratio)
+static int get_texture_color(t_texture texture, float h_ratio, float w_ratio)
 {
 	char	*pix;
 	t_pos	pix_pos;
 
 	pix_pos.x = texture.img->width * w_ratio;
-	pix_pos.y = texture.img->height * (0.5 - h_ratio);
+	pix_pos.y = texture.img->height * (0.5 + h_ratio);
 	pix = texture.img.data;
 	pix += pix_pos.y * texture.img->size_line;
 	pix += pix_pos.x * (texture.img->bpp / 8);
-	return (*(unsigned int *)pix);
+	return (*(int *)pix);
 }
 
 static t_texture	get_texture(t_RC tools_RC, t_global *global)
@@ -48,13 +47,13 @@ static float	get_w_ratio(t_RC tools_RC, t_global *global)
 	if (tools_RC.wall.horizontal)
 	{
 		if (tools_RC.ray.y < 0)
-			return (wall_pos.x - (int)wall_pos.x);
+			return (wall_pos.x - floor(wall_pos.x));
 		else
-			return (1.0 - (wall_pos.x - (int)wall_pos.x));
+			return (1.0 - (wall_pos.x - floor(wall_pos.x)));
 	}
 	if (tools_RC.ray.x > 0)
-		return (wall_pos.y - (int)wall_pos.y);
-	return (1 - (wall_pos.y - (int)wall_pos.y));
+		return (wall_pos.y - floor(wall_pos).y);
+	return (1 - (wall_pos.y - floor(wall_pos).y));
 }
 
 static void	draw_texture_pixel(int h, t_RC tools_RC, t_global *global)
@@ -67,14 +66,14 @@ static void	draw_texture_pixel(int h, t_RC tools_RC, t_global *global)
 	h_ratio = ((float)h) / tools_RC.wall_height;
 	w_ratio = get_w_ratio(tools_RC, global);
 	texture = get_texture(tools_RC, global);
-	pix_color = get_texture_color(texture, tools_RC, h_ratio, w_ratio);
-	put_pixel(global, tools_RC.s, (WIN_H / 2) - h, pix_color);
+	pix_color = get_texture_color(texture, h_ratio, w_ratio);
+	put_pixel(global, tools_RC.s, (WIN_H / 2) + h, pix_color);
 }
 
 void	draw_column(t_RC tools_RC, t_global *global)
 {
-	unsigned int	distance;
-	int				i;
+	float	distance;
+	int		i;
 
 	distance = compute_distance(tools_RC, global->myself->dir);
 	tools_RC.wall_height = HEIGHT_MOD * WIN_H / distance;
